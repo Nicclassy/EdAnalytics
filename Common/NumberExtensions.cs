92ced2eba366fb67069fea8f9be15997bc3cf46f
Nicclassy <lucanapoli122@gmail.com>
2024-11-10 21:58:18 +1100

    mod: more extensions and refactoring

--- Common/NumberExtensions.cs
+++ Common/NumberExtensions.cs
@@ -4,6 +4,8 @@ namespace Ed.Analytics.Common;
 
 public static class NumberExtensions
 {
-    public static double PercentageOf<T, U>(this T whole, U part) where T: INumber<T> where U: INumber<U> =>
-        Math.Round(Convert.ToDouble(whole) / Convert.ToDouble(part) * 100, 2);
+    public static double PercentageOf<T, U>(this T part, U whole) where T: INumber<T> where U: INumber<U> =>
+        whole == U.Zero ? 0 : Math.Round(Convert.ToDouble(part) / Convert.ToDouble(whole) * 100, 2);
+
+    public static string TwentyFourHourToTwelveHour(this int hour) => hour > 12 ? $"{hour - 12}pm" : $"{hour}am";
 }
--- Common/StringExtensions.cs
+++ Common/StringExtensions.cs
@@ -6,4 +6,20 @@ public static class StringExtensions
 {
     public static int CountCaseInsensitive(this string source, string value) =>
         Regex.Count(source, Regex.Escape(value), RegexOptions.IgnoreCase);
+
+    public static bool ContainsEmoji(this string source) =>
+        Enumerable.Range(0, source.Length).Any(index => 
+        {
+            var codePoint = char.IsSurrogatePair(source, index) 
+                ? char.ConvertToUtf32(source, index++)
+                : char.ConvertToUtf32(source, index);
+            return (codePoint >= 0x1F600 && codePoint <= 0xF614F) || 
+                    (codePoint >= 0x1F300 && codePoint <= 0x1F5FF) || 
+                    (codePoint >= 0x1F680 && codePoint <= 0x1F6FF) ||
+                    (codePoint >= 0x2600 && codePoint <= 0x26FF) ||
+                    (codePoint >= 0x2700 && codePoint <= 0x27BF) ||
+                    (codePoint >= 0xFE00 && codePoint <= 0xFE0F) ||
+                    (codePoint >= 0x1F900 && codePoint <= 0x1F9FF) ||
+                    (codePoint >= 0x1F1E6 && codePoint <= 0x1F1FF);
+        });
 }
